c15461492b650906a77f0ee05ec7f2ed9feef3c0
Masaaki Hamaya <admin@hamayamasaaki.com>
2025-08-12 22:12:09 +0900

    Update MealRecord.js

--- src/MealRecord.js
+++ src/MealRecord.js
@@ -1,4 +1,4 @@
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from './contexts/AuthContext';
 import React, { useState, useEffect } from 'react';
 import { db } from './firebase';
 import { 
