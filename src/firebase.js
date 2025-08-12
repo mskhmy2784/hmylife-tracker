6dcfafd8f3c2322b049fa9df27b37aef632ce31b
Masaaki Hamaya <admin@hamayamasaaki.com>
2025-08-12 22:02:36 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth'; // ← この行を追加
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHqDwddHDwmLHQe9WGEl6Kz2Luz1lDnBo",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app); // ← この行を追加
